91ee429eb6970d826a04ce7598c33278fa0c5029
Isuu1 <jakub.babelek@gmail.com>
2024-07-19 00:37:24 +0100

    Added styles for sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,7 +1,42 @@
 import React from "react";
 
+//Icons
+import { IoHome } from "react-icons/io5";
+import { FaTasks } from "react-icons/fa";
+import { FaDiagramProject } from "react-icons/fa6";
+import { FaNoteSticky } from "react-icons/fa6";
+import { IoSettings } from "react-icons/io5";
+
 const Sidebar = () => {
-  return <div className="sidebar">Sidebar</div>;
+  return (
+    <div className="sidebar">
+      <div>
+        <h2 className="sidebar__user-info">DailyPlanner</h2>
+      </div>
+      <ul className="sidebar__menu">
+        <li className="sidebar__menu__item">
+          <IoHome />
+          <p>Home</p>
+        </li>
+        <li className="sidebar__menu__item">
+          <FaTasks />
+          Tasks
+        </li>
+        <li className="sidebar__menu__item">
+          <FaDiagramProject />
+          Projects
+        </li>
+        <li className="sidebar__menu__item">
+          <FaNoteSticky />
+          Notes
+        </li>
+        <li className="sidebar__menu__item">
+          <IoSettings />
+          Settings
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Sidebar;
