e1f39f3caed0c91fd5caa6aa84e8c556d66bb6b1
Isuu1 <jakub.babelek@gmail.com>
2024-07-21 14:32:32 +0100

    Added note component and add note button

---
+++ src/components/Note/Note.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+//Icons
+import { FaLock } from "react-icons/fa";
+import { IoIosRemoveCircle } from "react-icons/io";
+
+const Note = () => {
+  const note = {
+    title: "Test note",
+    content:
+      "This note is for testing purposes only, just writing something to make a content longer to see how the text behave.",
+    backgroundColor: "#f5f77e",
+    textColor: "#00000",
+  };
+
+  const noteContainer = document.querySelector(".note");
+  noteContainer.style.backgroundColor = note.backgroundColor;
+
+  return (
+    <div className="note">
+      <button className="note__pin-button">
+        <FaLock />
+      </button>
+      <button className="note__remove-button">
+        <IoIosRemoveCircle />
+      </button>
+      <h2>{note.title}</h2>
+      <p>{note.content}</p>
+    </div>
+  );
+};
+
+export default Note;
--- src/pages/Notes/Notes.jsx
+++ src/pages/Notes/Notes.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import Note from "../../components/Note/Note";
 
 const Notes = () => {
-  return <div>Notes</div>;
+  return (
+    <div className="notes">
+      <Note />
+      <button className="notes__addnew-button">+</button>
+    </div>
+  );
 };
 
 export default Notes;
